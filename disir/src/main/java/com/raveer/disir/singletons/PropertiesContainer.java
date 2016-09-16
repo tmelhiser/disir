71a47b18fe172a8b63fcb4f9c20fa91f53e7f8e4
tmelhiser <tmelhiser@10.0.20.124>
2016-09-15 22:40:44 -0500

    Added tracking to know which location loaded the properties

--- disir/src/main/java/com/raveer/disir/singletons/PropertiesContainer.java
+++ disir/src/main/java/com/raveer/disir/singletons/PropertiesContainer.java
@@ -52,6 +52,10 @@ import com.raveer.disir.utils.StringUtils;
 public enum PropertiesContainer {
 	INSTANCE;
 	
+	public static final Integer LOAD_FAILED = 0;
+	public static final Integer LOAD_FROM_SYSTEM_PATH = 1;
+	public static final Integer LOAD_FROM_CLASSPATH = 2;
+	public static final Integer LOAD_FROM_DATABASE = 3;
 	private final HashMap<String,Properties> propertiesMap = new HashMap<String, Properties>();
 	private final HashMap<String,Long> propertiesMapAge = new HashMap<String, Long>();
 	private static final Logger LOGGER = Logger.getLogger(PropertiesContainer.class.getName());
@@ -62,35 +66,45 @@ public enum PropertiesContainer {
 
 	public Properties getPropertiesByCoordinate(String jndiDBName, String dbTableName, String sql, String nameSpace, String propertiesFile, Boolean preferPropertiesFile) {
 		String propertiesMapKey = StringUtils.join(Arrays.asList(new String[] {jndiDBName, dbTableName, sql, nameSpace, propertiesFile, preferPropertiesFile.toString()}), "###");
-		Properties targetProperties = propertiesMap.get(propertiesMapKey); 
+		Properties properties = propertiesMap.get(propertiesMapKey);
 				
-		if (targetProperties==null) {
+		if (properties==null) {
 			synchronized (this) {
-				targetProperties = propertiesMap.get(propertiesMapKey);
-				if ( targetProperties == null) {
+				properties = propertiesMap.get(propertiesMapKey);
+				if ( properties == null) {
+					properties = new Properties();
+					int returnedFrom = LOAD_FAILED;
 					LOGGER.info("Trying to create Cache Property Coordinate: " + propertiesMapKey);
-					targetProperties = new Properties();
+					
 					if (preferPropertiesFile) {
-						newNameSpaceFromFiles(propertiesFile,targetProperties);
-						if (targetProperties.isEmpty()) {
+						returnedFrom = newNameSpaceFromFiles(propertiesFile,properties);
+						if (properties.isEmpty()) {
 							LOGGER.severe("No values loaded from file (" + propertiesFile +"): Trying Database");
-							newNameSpaceFromDB(jndiDBName, dbTableName, sql, nameSpace,targetProperties);
+							returnedFrom = newNameSpaceFromDB(jndiDBName, dbTableName, sql, nameSpace, properties);
 						}
 					} else {
-						newNameSpaceFromDB(jndiDBName, dbTableName, sql, nameSpace,targetProperties);
-						if (targetProperties.isEmpty()) {
+						returnedFrom = newNameSpaceFromDB(jndiDBName, dbTableName, sql, nameSpace,properties);
+						if (properties.isEmpty()) {
 							LOGGER.severe("No values lodaed from Database (" + jndiDBName + ") Table (" + dbTableName + "): Trying files");
-							newNameSpaceFromFiles(propertiesFile,targetProperties);
+							returnedFrom = newNameSpaceFromFiles(propertiesFile,properties);
 						}
 					}
-					if (!targetProperties.isEmpty()) {
+					if (!properties.isEmpty()) {
 						LOGGER.info("Caching Property Coordinate: " + propertiesMapKey);
 						if (LOGGER.getLevel() == Level.FINEST) {
-							for (Entry<?, ?> entry : targetProperties.entrySet()) {
-								LOGGER.finest("Properties for NameSpace ("+propertiesMapKey+"): " + entry.getKey() +"=" + entry.getValue());
+							for (Entry<?, ?> entry : properties.entrySet()) {
+								if (returnedFrom == LOAD_FROM_SYSTEM_PATH) {
+									LOGGER.finest("Properties from Classpath (" + propertiesFile +") for NameSpace ("+propertiesMapKey+"): " + entry.getKey() +"=" + entry.getValue());
+								} else if (returnedFrom == LOAD_FROM_CLASSPATH) {
+									LOGGER.finest("Properties from File System (" + propertiesFile +") NameSpace ("+propertiesMapKey+"): " + entry.getKey() +"=" + entry.getValue());
+								} else if (returnedFrom == LOAD_FROM_DATABASE) {
+									LOGGER.finest("Loaded from DB (" + jndiDBName + ") Table ("+dbTableName+") Properties for NameSpace ("+propertiesMapKey+"): " + entry.getKey() +"=" + entry.getValue());
+								} else {
+									LOGGER.finest("Loaded Properties for Coordinate (" + propertiesMapKey +") : " + entry.getKey() +"=" + entry.getValue());
+								}
 							}
 						}
-						propertiesMap.put(propertiesMapKey, targetProperties);
+						propertiesMap.put(propertiesMapKey, properties);
 						propertiesMapAge.put(propertiesMapKey, System.currentTimeMillis());
 					} else {
 						LOGGER.severe("No entries found! NOT Caching: " + propertiesMapKey);
@@ -99,7 +113,7 @@ public enum PropertiesContainer {
 			};
 		}
 		
-		return targetProperties;
+		return properties;
 	}
 	
 	public void refreshNameSpace(String refreshNameSpace) {
@@ -112,48 +126,52 @@ public enum PropertiesContainer {
 		}
 	}
 
-	private void newNameSpaceFromDB(String jndiDBName, String dbTableName, String sql, String nameSpace, Properties targetProperties) {
+	private Integer newNameSpaceFromDB(String jndiDBName, String dbTableName, String sql, String nameSpace, Properties targetProperties) {
 		LOGGER.info("Trying to load NameSpace ("+nameSpace+") from Database DB ("+jndiDBName+") Table: " + dbTableName);
-			try {
-				Context context = new InitialContext();
-				DataSource dataSource = (DataSource) context.lookup(jndiDBName);
-				try (Connection connection = dataSource.getConnection()) {
-					try(PreparedStatement prep = connection.prepareStatement(String.format(sql,dbTableName))) {
-						prep.setString(1, nameSpace);
-						
-						try(ResultSet rs = prep.executeQuery()) {
-							while(rs.next()) {
-								String key = rs.getString("key");
-								String value = rs.getString("value");
-								targetProperties.put(key, value);
-							}
+		try {
+			Context context = new InitialContext();
+			DataSource dataSource = (DataSource) context.lookup(jndiDBName);
+			try (Connection connection = dataSource.getConnection()) {
+				try(PreparedStatement prep = connection.prepareStatement(String.format(sql,dbTableName))) {
+					prep.setString(1, nameSpace);
+					
+					try(ResultSet rs = prep.executeQuery()) {
+						while(rs.next()) {
+							String key = rs.getString("key");
+							String value = rs.getString("value");
+							targetProperties.put(key, value);
 						}
 					}
-					LOGGER.info("Loaded from NameSpace ("+nameSpace+") DB ("+jndiDBName+") Table ("+ dbTableName +"): " + targetProperties.size());
-				} catch (SQLException e) {
-					LOGGER.severe("Error connecting to DataBase: " + jndiDBName);
-					LOGGER.severe("Database Stack Trace: " + e);
 				}
-			} catch (NamingException e) {
+				LOGGER.info("Loaded from NameSpace ("+nameSpace+") DB ("+jndiDBName+") Table ("+ dbTableName +"): " + targetProperties.size());
+			} catch (SQLException e) {
 				LOGGER.severe("Error connecting to DataBase: " + jndiDBName);
 				LOGGER.severe("Database Stack Trace: " + e);
 			}
+		} catch (NamingException e) {
+			LOGGER.severe("Error connecting to DataBase: " + jndiDBName);
+			LOGGER.severe("Database Stack Trace: " + e);
+		}
+		return LOAD_FROM_DATABASE;
 	}
 
-	private void newNameSpaceFromFiles(String propertiesFile, Properties targetProperties) {
+	private Integer newNameSpaceFromFiles(String propertiesFile, Properties targetProperties) {
 		LOGGER.info("Trying to load from Properties File: " + propertiesFile);
 		try (InputStream stream1 = new FileInputStream(propertiesFile)) {
 			targetProperties.load(stream1);
 			LOGGER.info("Loaded from filesystem (" + propertiesFile +"): " + targetProperties.size());
+			return LOAD_FROM_SYSTEM_PATH;
 		} catch (Exception e1) {
 			LOGGER.severe("Unable to find Properties File on filesystem, trying classpath: " + e1);
 			try (InputStream stream2 = this.getClass().getResourceAsStream(propertiesFile)) {
 				targetProperties.load(stream2);
 				LOGGER.info("Loaded from classpath (" + propertiesFile +"): " + targetProperties.size());
+				return LOAD_FROM_CLASSPATH;
 			} catch (Exception e2) {
 				LOGGER.severe("No properties loaded from file: " + e2);
 			}
 		}
+		return LOAD_FAILED;
 	}
 
 	public Runnable refreshNameSpaces() {	
